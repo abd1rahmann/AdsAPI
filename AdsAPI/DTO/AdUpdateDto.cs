e20374306a0ca17068b47d3391a8aa9e10540230
abd1rahmann <abdi123rahman@outlook.com>
2025-06-10 06:42:20 +0200

    change of structure

--- AdsAPI/DTO/AdUpdateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AdsAPI.DTO
-{
-    public class AdUpdateDto
-    {
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-    }
-}
